f9f473901129815626ef30fec3e4179e833c3def
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2023-09-21 18:05:33 +0200

    Lab 4 adjustments

--- experiments/soundExperiment.js
+++ experiments/soundExperiment.js
@@ -1,18 +1,14 @@
+
+
 let ready = false;
 let osc;
 
-function setup() {
-  createCanvas(innerWidth, innerHeight);
-  osc = new Tone.Oscillator();
-  osc.connect(Tone.Master);
-}
 
 function draw() {
 if (ready) {
 } else {
   //do stuff
   fill(255);
-  background(0);
   noStroke();
   textAlign(CENTER, CENTER);
   text("CLICK TO START", width/2, height/2);
